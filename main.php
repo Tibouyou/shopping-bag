5d986490842879b73ca83e82b2879f7c26bdcdc2
Clara <clara.sithidej@etu.univ-lyon1.fr>
2023-11-26 15:00:20 +0100

    premiers affichage d'une page

---
+++ main.php
@@ -0,0 +1,23 @@
+<?php
+
+/* récupérer le tableau des données */
+require 'models/model.php';
+$donnees = getData();
+
+/* inclure l'autoloader */
+require_once 'vendor/autoload.php';
+
+/* templates chargés à partir du système de fichiers (répertoire vue) */
+$loader = new Twig\Loader\FilesystemLoader('vue');
+
+/* options : prod = cache dans le répertoire cache, dev = pas de cache */
+$options_prod = array('cache' => 'cache', 'autoescape' => true);
+$options_dev = array('cache' => false, 'autoescape' => true);
+
+/* stocker la configuration */
+$twig = new Twig\Environment($loader);
+
+/* charger+compiler le template, exécuter, envoyer le résultat au navigateur */
+echo $twig->render('vue_accueil.twig', $donnees);
+?>
+
---
+++ models/model.php
@@ -0,0 +1,14 @@
+<?php
+function getData()
+{
+    $data = array(
+            'biblio' => array(
+            array('titre' => 'N ou M', 'nom' => 'Christie', 'prenom' => 'Agatha'),
+            array('titre' => '1984', 'nom' => 'Orwell', 'prenom' => 'George'),
+            array('titre' => 'Dune', 'nom' => 'Herbert', 'prenom' => 'Frank')
+        )
+    );
+    return $data;
+}
+
+?>
