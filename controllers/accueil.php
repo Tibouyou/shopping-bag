f4bf41ec2c574223c425218b0bfbdcd84ece8cbe
Tibouyou <tibouyou01@gmail.com>
2023-12-16 13:36:01 +0100

    Add to cart function

--- controllers/accueil.php
+++ controllers/accueil.php
@@ -2,17 +2,14 @@
 require_once('models/products.php');
 class ControleurAccueil
 {
-    private $products;
     private $twig;
     public function __construct($twig)
     {
-        $this->products = new Products();
         $this->twig = $twig;
     }
     public function accueil()
     {
-        echo $this->twig->render('accueil.twig', array(
-            'products' =>$this->products->get_all_products()->fetchAll()));
+        echo $this->twig->render('accueil.twig');
     }
 }
 ?>
