78562a4be61078c81a8a322ca947d412c5bc8e3a
Tibouyou <tibouyou01@gmail.com>
2023-12-23 14:16:10 +0100

    Convert category links to lowercase

--- controllers/accueil.php
+++ controllers/accueil.php
@@ -13,7 +13,7 @@ class ControleurAccueil
     {
         $all_categories = $this->categories->get_all_categories()->fetchAll();
         for ($i = 0; $i < count($all_categories); $i++) {
-           $all_categories[$i]['link'] = str_replace(' ', '-', $all_categories[$i]['name']);
+           $all_categories[$i]['link'] = strtolower(str_replace(' ', '-', $all_categories[$i]['name']));
         }
         echo $this->twig->render('accueil.twig' , array(
             'categories' => $all_categories
